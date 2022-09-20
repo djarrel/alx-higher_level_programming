1f04757addc56b42b1470c262c49b058426fc147
djarrel <jeffdarrellthegreat@gmail.com>
2022-09-20 12:13:17 -0700

    print combinatio of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(0, 10):
     for n in range(i + 1, 10):
         if i == 8 and n == 9:
             break
-        print("{:d}{:d}".format(i, n), end = ', ')
+        print("{:d}{:d}".format(i, n), end=', ')
 print("89")
