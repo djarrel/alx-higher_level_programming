be813abf1607cb06ba810abb7856edc9bc99b3ae
djarrel <jeffdarrellthegreat@gmail.com>
2022-09-20 23:54:36 -0700

    print reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for r in range(122, 96, -1):
-    print("{:c}".format(r), end="")
+    if r % 2 == 0:
+        print("{:c}".format(r), end="")
+    else:
+        print("{:c}".format(r - 32), end="")
