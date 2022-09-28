794941bc3adb011888e7ccde2e036a0a0ff765a6
djarrel <jeffdarrellthegreat@gmail.com>
2022-09-28 13:31:25 -0700

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,14 @@ import sys
 
 
 def main():
-    l = len(sys.argv)
-    print('{:d} argument{:}'.format(l - 1, '.' if l == 1 else
-                                    (':' if l == 2 else 's:')))
+    length = len(sys.argv)
+    print('{:d} argument{:}'.format(length - 1, '.' if length == 1 else
+                                    (':' if length == 2 else 's:')))
     i = 1
     for arg in sys.argv[1:]:
         print("{:d}: {}".format(i, arg))
         i += 1
+
 
 if __name__ == "__main__":
     main()
